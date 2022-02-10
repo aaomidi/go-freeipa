5ab293a02212ef91a4f9492a4e3af1b80acf7669
Amir Omidi <amir@aaomidi.com>
2022-02-10 06:55:53 +0000

    Update latest info

--- dump-errors/dump.py
+++ dump-errors/dump.py
@@ -5,7 +5,7 @@ import re
 import inspect
 import json
 
-ERRORS_PY_URL = "https://raw.githubusercontent.com/freeipa/freeipa/e1bd827bbf56970ddd02ec174bf2317b64e75514/ipalib/errors.py"
+ERRORS_PY_URL = "https://raw.githubusercontent.com/freeipa/freeipa/61650c577f8847063de73218e1b8bc9f55e62fd8/ipalib/errors.py"
 
 import_regex = re.compile(r"^(from [\w\.]+ )?import \w+( as \w+)?$")
 
